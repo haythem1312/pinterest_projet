5be33355a319db3b213b42d4a96482be64fd5ae7
heni haythem <donfather25@gmail.com>
2021-04-30 17:56:31 +0200

    Add relation between pinsand users

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -65,6 +65,12 @@ class Pin
      */
     private $imageName;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="pins")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
   
 
     public function getId(): ?int
@@ -158,6 +164,18 @@ $this->setUpdatedAt( new \DateTimeImmutable);
     public function setImageName(?string $imageName): self
     {
         $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
