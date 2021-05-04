a8dd9fd95d2a6077790312724943a1f5f47e52bf
heni haythem <donfather25@gmail.com>
2021-05-05 01:49:56 +0200

    More robust change password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -10,11 +10,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+/**
+ * @Route("/account")
+ */
+
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account",methods="GET")
+     * @Route("", name="app_account",methods="GET")
      */
     public function sohw(): Response
     {
@@ -23,7 +27,7 @@ class AccountController extends AbstractController
     }
 
      /**
-     * @Route("/account/edit", name="app_account_edit",methods={"GET","POST"})
+     * @Route("/edit", name="app_account_edit",methods={"GET","POST"})
      */
     public function edit(Request $request,EntityManagerInterface $em): Response
 
@@ -40,12 +44,12 @@ class AccountController extends AbstractController
 return $this->render('account/edit.html.twig' , ['form'=>$form->createView()]);
 }
   /**
-     * @Route("/account/change-password", name="app_account_change_password",methods={"GET","POST"})
+     * @Route("/change-password", name="app_account_change_password",methods={"GET","POST"})
      */
     public function changePassword(Request $request,EntityManagerInterface $em,UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user=$this->getUser();
-    $form=$this->createForm(ChangePasswordFormType::class);
+    $form=$this->createForm(ChangePasswordFormType::class,null,['current_password_is_required'=>true]);
     $form->handleRequest($request);
     if ($form->isSubmitted()&& $form->isValid()) {
         $user->setPassword($passwordEncoder->encodePassword($user,$form['plainPassword']->getData()));
