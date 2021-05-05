84d79f9faa6350ac5a8133fb3452e23b9086c890
heni haythem <donfather25@gmail.com>
2021-05-05 05:12:28 +0200

    Display flash message on logout

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -128,8 +128,8 @@ class ResetPasswordController extends AbstractController
 
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
-
-            return $this->redirectToRoute('app_home');
+$this->addFlash('success','Your password has been successfully reset you can log in with the new password');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('reset_password/reset.html.twig', [
